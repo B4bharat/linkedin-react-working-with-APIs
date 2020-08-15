415fb2aae00dda5798bb0d66f81a2cd318da2ac2
Bharat Poptwani <b4bharat.available@gmail.com>
2020-08-15 18:15:01 +0530

    set up the REST API to fetch articles

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,6 +2,31 @@ import React, { Component } from "react";
 import NewSingle from "./NewSingle";
 
 class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: []
+    };
+  }
+
+  componentDidMount() {
+    const url =
+      "http://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=bfef6454abcc4607acaf521ee9e53e13";
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          news: data.articles
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   renderItems() {
     return this.props.items.map((item) => {
       <NewSingle key={item.id} item={item} />;
